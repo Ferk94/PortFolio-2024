23c2e6d3595af7c8b72f680b10ec81ba91358c34
Ferk94 <fernando.kaganovicz@gmail.com>
2026-08-21 17:34:42 -0300

    change theme to prime

--- app/layout.tsx
+++ app/layout.tsx
@@ -49,7 +49,7 @@ export default function RootLayout({
     script.setAttribute('data-pwa-url', 'http://test.appjugando.com/platense');
     script.setAttribute('data-host-auth-enabled', 'true');
     script.setAttribute('data-gift-url', 'https://promociones.com');
-    script.setAttribute('data-brand-theme', 'hivara');
+    script.setAttribute('data-brand-theme', 'prime');
     script.setAttribute('data-embed-version', 'v1.0.4');
 
     // 4. Inyectamos el script en el documento
